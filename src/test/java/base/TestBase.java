2825bb679e7af2a9bb9d43e2acf9ebb15989871a
Minskoleopc <minskoleopc@gmail.com>
2022-12-17 06:56:29 +0530

    added

---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,46 @@
+package base;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties pro = new Properties();
+	
+	@BeforeTest
+	public void SetUp() throws IOException {
+		if(driver == null) {
+			//System.out.println(System.getProperty("user.dir"));
+			FileInputStream input = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\configFiles\\config.properties");
+			pro.load(input);
+			System.out.println(pro.getProperty("browser"));
+		}
+		
+		if(pro.getProperty("browser").equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.get(pro.getProperty("baseUrl"));
+		}
+		else if(pro.getProperty("browser").equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.get(pro.getProperty("baseUrl"));
+		}
+	}
+	
+	@AfterTest
+	public void TearDown() {
+		driver.quit();	
+	}
+	
+}
---
+++ src/test/java/testcases/TestOne.java
@@ -0,0 +1,33 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import base.TestBase;
+
+public class TestOne extends TestBase {
+	
+	@Test
+	public static void Login() {
+		driver.manage().window().maximize();
+		driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+		driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+		driver.findElement(By.cssSelector("#login-button")).click();
+		
+	}
+	@Test
+	public static void CountItems() {
+	
+	}
+	@Test
+	public static void GetTitle() {
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
